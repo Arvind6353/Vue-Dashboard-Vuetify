cb0cec27cac018b8d223e9b365501007453f8e39
T A <arvta@paypal.com>
2018-02-01 10:23:19 +0530

    confluence changes

--- src/components/Confluence.vue
+++ src/components/Confluence.vue
@@ -6,39 +6,85 @@
    </v-toolbar-title>
    <v-spacer class="hidden-xs-only"></v-spacer>
 
-      <!-- <v-text-field
-         light
-         solo
-         prepend-icon="search"
-         placeholder="Search...">
-      </v-text-field> -->
 
-      <v-select
+      <!-- <v-select
                 label="Start typing..."
                 autocomplete
                 :loading="loading"
                 light
                 solo
-                multiple
                 cache-items
                 chips
                 :items="items"
                 :search-input.sync="search"
                 v-model="select"
-              ></v-select>
+              >
+              </v-select> -->
+
+
+               <v-select
+                label="Start Typing ..."
+                v-bind:items="items"
+                :loading="loading"
+                v-model="e11"
+                light
+                solo
+                chips
+                item-text="name"
+                item-value="name"
+                max-height="auto"
+                autocomplete
+                @input.native = "querySelections2"
+              >
+              <template slot="selection" slot-scope="data">
+
+                  <v-chip
+                    @input="data.parent.selectItem(data.item)"
+                    :selected="data.selected"
+                    class="chip--select-multi"
+                    :key="JSON.stringify(data.item)"
+                  >
+                    {{ data.item.name }}           <v-icon
+                    @click="closeThis">close</v-icon>
+
+                  </v-chip>
+                </template>
+
+                <template slot="item" slot-scope="data">
+                  <template v-if="typeof data.item !== 'object'">
+                  </template>
+                  <template v-else>
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
+                      <v-list-tile-sub-title v-html="data.item.group"></v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </template>
+                </template>
+               </v-select>
 </v-toolbar>
 
 </div>
 </template>
 
 <script>
+import debounce from "debounce";
+
 export default {
-  name: 'confluence',
-  data () {
+  name: "confluence",
+  data() {
+    let srcs = {
+      1: "/static/doc-images/lists/1.jpg",
+      2: "/static/doc-images/lists/2.jpg",
+      3: "/static/doc-images/lists/3.jpg",
+      4: "/static/doc-images/lists/4.jpg",
+      5: "/static/doc-images/lists/5.jpg"
+    };
+
     return {
       loading: false,
       items: [],
       search: null,
+      search1: null,
       select: [],
       states: [
         "Alabama",
@@ -100,27 +146,94 @@ export default {
         "West Virginia",
         "Wisconsin",
         "Wyoming"
+      ],
+      e11: [],
+      people: [
+        { name: "Sandra Adams", group: "Group 1", avatar: srcs[1] },
+        { name: "Ali Connors", group: "Group 1", avatar: srcs[2] },
+        { name: "Trevor Hansen", group: "Group 1", avatar: srcs[3] },
+        { name: "Tucker Smith", group: "Group 1", avatar: srcs[2] },
+        { name: "Britta Holt", group: "Group 2", avatar: srcs[4] },
+        { name: "Jane Smith ", group: "Group 2", avatar: srcs[5] },
+        { name: "John Smith", group: "Group 2", avatar: srcs[1] },
+        { name: "Sandra Williams", group: "Group 2", avatar: srcs[3] }
       ]
-    }
+    };
   },
   watch: {
-    search (val) {
-      val && this.querySelections(val)
+    search1(val) {
+      if (val == "") {
+        this.querySelections1(val);
+        return;
+      } else if (val && val.length > 1) {
+        this.querySelections1(val);
+      }
     }
   },
   methods: {
-    querySelections (v) {
-      this.loading = true
+    querySelections(v) {
+      this.loading = true;
       // Simulated ajax query
       setTimeout(() => {
         this.items = this.states.filter(e => {
-          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
-        })
-        this.loading = false
-      }, 500)
+          return (e || "").toLowerCase().indexOf((v || "").toLowerCase()) > -1;
+        });
+        this.loading = false;
+      }, 500);
+    },
+    querySelections1(v) {
+      this.loading = true;
+      console.log("v --", v);
+
+      // Simulated ajax query
+      setTimeout(() => {
+        if (v == "") {
+          this.items = this.people.slice(0);
+          this.loading = false;
+          this.e11 = null;
+          return;
+        }
+        this.items = this.people.filter(e => {
+          return (
+            ((e && e.name) || "")
+              .toLowerCase()
+              .indexOf((v || "").toLowerCase()) > -1
+          );
+        });
+        this.loading = false;
+      }, 500);
+    },
+
+    querySelections2: debounce(function(event) {
+      this.loading = true;
+      var v = event.target.value;
+      console.log("v --", v);
+      // Simulated ajax query
+      setTimeout(() => {
+        if (v == "") {
+          this.items = this.people.slice(0);
+          this.loading = false;
+          this.e11 = null;
+          return;
+        }
+        if (event.target.value.length > 0) {
+          this.items = this.people.filter(e => {
+            return (
+              ((e && e.name) || "")
+                .toLowerCase()
+                .indexOf((v || "").toLowerCase()) > -1
+            );
+          });
+          this.loading = false;
+        }
+      }, 500);
+    }, 200),
+    closeThis() {
+      this.items = this.people.splice();
+      this.e11 = null;
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
