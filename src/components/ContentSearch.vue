5f5a94e5355fbed9ff6d8841c69b1b6eb565d98f
T A <arvta@paypal.com>
2018-02-10 23:54:34 +0530

    server side pagination changes

--- src/components/ContentSearch.vue
+++ src/components/ContentSearch.vue
@@ -2,6 +2,8 @@
 <div id="app">
 
   <ContentSearchNav></ContentSearchNav>
+
+
   <v-card>
       <v-card-title>
         <span class="title"></span>
@@ -16,6 +18,10 @@
         ></v-text-field>
       </v-card-title>
 
+
+<br/>
+<!--
+  {{pagination}} <br/> -->
      <!-- v-bind:search="search" -->
      <v-data-table
         v-model="selected"
@@ -49,27 +55,57 @@
       <template slot="items" slot-scope="props">
         <tr :active="props.selected" >
 
-          <td>{{ props.item.name }}</td>
+          <td>
+             <v-icon :color="getColor(props.item.name)">
+                              {{props.item.name | icon}}
+                          </v-icon>
+            &nbsp;&nbsp; {{ props.item.name }}</td>
           <td>{{ props.item.id }}</td>
-          <td>{{ props.item.url }}</td>
+          <td>
+            <span class="blue--text text--darken-4">
+            {{ props.item.url | urlcheck}}
+            </span>
+            </td>
         </tr>
       </template>
-        <template slot="no-data">
+
+      <template slot="no-data">
         <v-alert :value="true" :color="loadingColor" :icon="loadingIcon">
           {{loadingMsg}}
         </v-alert>
       </template>
 
+    <template slot="no-results">
+        <v-alert :value="true" :color="loadingColor" :icon="loadingIcon">
+          Loading More data
+        </v-alert>
+      </template>
+
+
 
     </v-data-table>
-    {{pagination}} <br/>
+
+<br/><v-card-title>
+<v-flex xs3>
+            <v-select
+              v-bind:items="[5,10,20,25]"
+              v-model="pagination.rowsPerPage"
+              label="Items Per Page"
+              class="input-group--focused"
+            ></v-select>
+    </v-flex>
+    <v-flex offset-xs-2>
      <v-pagination v-model="pagination.page" :length="pages" :circle="true"
+     :total-visible="15"
      ></v-pagination>
+    </v-flex>
+</v-card-title>
+<br/>
+<br/>
     </v-card>
   </div>
 
 </template>
-
 
 
 <script>
@@ -97,33 +133,23 @@ export default {
       delData: {},
       headers: [
         {
-          text: 'Title',
-          align: 'left',
+          text: 'TITLE',
           value: 'name'
         },
-        { text: 'Doc Id', value: 'id' },
-        { text: 'Url', value: 'url' }
+        { text: 'DOC ID', value: 'id' },
+        { text: 'URL', value: 'url' }
 
       ],
       items: []
     }
   },
- watch: {
-    pagination: {
-      handler (newVal) {
-        console.log("new vla ", newVal);
-        // alert(newVal)
-        // if(newVal.rowsPerPage * newVal.page > this.items.length && newVal.page !=1 &&
-
-        // newVal.rowsPerPage * newVal.page < this.totalItems ){
-        //   let offset = this.items.length + 1;
-        //     this.callApi(offset);
-
-        // }
-
-      }
-    }
-  },
+//  watch: {
+//     pagination: {
+//       handler (newVal) {
+//         console.log("new vla ", newVal);
+//       }
+//     }
+//   },
 
   mounted () {
     // this.loadData();
@@ -136,9 +162,10 @@ export default {
       return value.join(',');
     }
   },
-computed: {
+  computed: {
     pages () {
       var newVal = this.pagination;
+
       if(newVal.rowsPerPage * newVal.page > this.items.length && newVal.page !=1 &&
 
         newVal.rowsPerPage * newVal.page < this.totalItems ){
@@ -151,6 +178,7 @@ computed: {
       return this.pagination.rowsPerPage ? Math.ceil(this.totalItems / this.pagination.rowsPerPage) : 0
     }
   },
+
   methods: {
     changeSort (column) {
       if (this.pagination.sortBy === column) {
@@ -162,6 +190,12 @@ computed: {
     },
     loadData : debounce(function(event,offset) {
 
+      this.pagination.totalItems = 0;
+      this.pagination.page = 1;
+      this.pagination.sortBy = "name";
+      this.pagination.descending = false;
+      this.totalItems = 0;
+      this.items = [];
       if(!offset){
           offset = 0;
         }
@@ -177,46 +211,112 @@ computed: {
       this.loadingIcon = "info";
       this.loadingMsg = "Loading Data";
       this.getCustomerData(this.search, offset)
-        .then(data => {
-              this.loading = false
+        .then(result => {
+            var data = result.data;
+            if(data) {
               this.pagination.totalItems = data.totalItems;
               this.totalItems = data.totalItems;
 
               this.items = [... this.items,  ...data.searchResults.map((entry,index) => {
                  return { name : entry.name, id: entry.id, url : entry.url, idx:index }
                })]
+            }
+            this.loading = false
+            if(!this.items || this.items.length == 0) {
+              this.loadingMsg = "No data found";
+              this.loadingColor = "red";
+              this.loadingIcon = "warning";
+            }
+      }).catch(error => {
+          this.loading = false;
+          this.items = [];
+          this.totalItems = 0;
+          this.loadingMsg = "Some Error Occurred . Please try again!";
+          this.loadingColor = "red";
+          this.loadingIcon = "warning";
+          console.error(error);
+          //this.$router.push("errorpage");
+        });
 
-              if(!this.items || this.items.length == 0) {
-                this.loadingMsg = "No data found";
-                this.loadingColor = "red";
-                this.loadingIcon = "warning";
-              }
-      })
     },
+
     getCustomerData (param, offset) {
         if(!param) {
           param = "search"
         }
-
-
         this.loading = true
-        return this.$http.get(config.serverUrl+"/customer/box/"+param+"?offset="+offset).then(result => {
-              return result.data;
+        return this.$http.get(encodeURI(config.serverUrl+"/customer/box/"+param+"?offset="+offset))
+    },
+    getColor( value){
+      if (!value) return ''
 
-        }, error => {
-            this.loading = false;
-            this.items = [];
-            this.totalItems = 0;
-            this.loadingMsg = "Some Error Occurred . Please try again!";
-            this.loadingColor = "red";
-            this.loadingIcon = "warning";
-            console.error(error);
-            this.$router.push("errorpage");
+      var reversedStr = value.split("").reverse().join("");
+      var iconType = reversedStr.split('.')[0].split("").reverse().join("").trim();
+      if(iconType == 'docx' || iconType == 'doc'){
+          return  'blue darken-2'
+      } else if(iconType == 'ppt' || iconType == 'pptx') {
+          return 'red'
+      } else if(iconType == 'xls' || iconType == 'xlsx' || iconType == 'xlsm') {
+          return 'green darken-2'
+      } else if(iconType == 'js' || iconType == 'json') {
+          return 'black darken-2'
+      } else if(iconType == 'css') {
+          return 'pink darken-2'
+      } else if(iconType == 'py') {
+          return 'black darken-2'
+      } else if(iconType == 'html') {
+          return 'blue darken-2'
+      } else if(iconType == 'text' || iconType == 'txt') {
+          return "blue lighten-1"
+      }  else if(iconType == 'pdf') {
+          return "red lighten-1"
+      } else if ( iconType == 'msg') {
+        return "black"
+      } else {
+        return "black lighten-2"
+      }
 
-        });
+
     }
+  },
+  filters: {
+    icon: function (value) {
+      if (!value) return ''
 
+      var reversedStr = value.split("").reverse().join("");
+      var iconType = reversedStr.split('.')[0].split("").reverse().join("").trim();
+
+      if(iconType == 'docx' || iconType == 'doc'){
+          return  'fa fa-file-word-o fa-x'
+      } else if(iconType == 'ppt' || iconType == 'pptx') {
+          return 'fa fa-file-powerpoint-o fa-x'
+      } else if(iconType == 'xls' || iconType == 'xlsx' || iconType == 'xlsm') {
+          return 'fa fa-file-excel-o fa-x'
+      }  else if(iconType == 'js' || iconType == 'json') {
+          return 'fa fa-file-code-o fa-x'
+      } else if(iconType == 'css') {
+          return 'fa fa-css3 fa-x'
+      } else if(iconType == 'py') {
+          return 'fa fa-file-code-o fa-x'
+      } else if(iconType == 'html') {
+          return 'fa fa-html5 fa-x'
+      } else if(iconType == 'text' || iconType == 'txt') {
+          return 'fa fa-file-text-o fa-x'
+      } else if(iconType == 'pdf') {
+          return 'fa fa-file-pdf-o fa-x'
+      } else if(iconType == 'msg') {
+          return 'fa fa-file-o fa-x'
+      } else {
+        return 'fa fa-file fa-x'
+      }
+
+    },
+    urlcheck : function(value) {
+      if(!value) return "-";
+      return value;
+    }
   }
+
 }
 
 </script>
